eef4ee649972b4f143f46eeee217ef38080d5ca4
sanchay <sanchaysaxena.22@gmail.com>
2024-04-11 15:24:58 +0530

    6

---
+++ app/src/main/java/com/example/todofirebase/fregments/AddTodoPopupFragment.kt
@@ -0,0 +1,19 @@
+package com.example.todofirebase.fregments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todofirebase.R
+
+class AddTodoPopupFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_todo_popup, container, false)
+    }
+
+}
--- app/src/main/java/com/example/todofirebase/fregments/HomeFragment.kt
+++ app/src/main/java/com/example/todofirebase/fregments/HomeFragment.kt
@@ -6,9 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.todofirebase.R
+import com.example.todofirebase.databinding.FragmentHomeBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class HomeFragment : Fragment() {
@@ -16,12 +19,32 @@ class HomeFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
     private lateinit var navController:NavController
     private lateinit var databaseRef:DatabaseReference
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding=FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        registerEvents()
+    }
+
+    private fun registerEvents() {
+        binding.addButtonHome.setOnClickListener {
+
+        }
+    }
+
+    private fun init(view: View) {
+        auth=FirebaseAuth.getInstance()
+        navController=Navigation.findNavController(view)
+        databaseRef=FirebaseDatabase.getInstance().reference
     }
 }
