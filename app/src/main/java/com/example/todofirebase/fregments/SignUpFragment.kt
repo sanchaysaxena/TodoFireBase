069dbdd54ae3578c914792a8d685905c9d3c4220
sanchay <sanchaysaxena.22@gmail.com>
2024-04-11 15:08:51 +0530

    5

--- app/src/main/java/com/example/todofirebase/fregments/SignUpFragment.kt
+++ app/src/main/java/com/example/todofirebase/fregments/SignUpFragment.kt
@@ -1,21 +1,78 @@
 package com.example.todofirebase.fregments
 
+import android.os.Binder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.todofirebase.R
+import com.example.todofirebase.databinding.FragmentSignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SignUpFragment : Fragment() {
+
+    private lateinit var auth:FirebaseAuth
+    private lateinit var navControl:NavController
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding=FragmentSignUpBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        init(view)
+        registerEvents()
+    }
+
+    private fun init(view: View) {
+        navControl=Navigation.findNavController(view)
+        auth=FirebaseAuth.getInstance()
+    }
+
+    private fun registerEvents() {
+
+        binding.authTextView.setOnClickListener{
+            navControl.navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+
+
+        binding.nextBtn.setOnClickListener {
+            val email = binding.emailEt.text.toString().trim()
+            val pass = binding.passlEt.text.toString().trim()
+            val verifyPass = binding.rePasslEt.text.toString().trim()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && verifyPass.isNotEmpty()) {
+                if (pass == verifyPass) {
+                    binding.progressBar.visibility=View.VISIBLE
+                    auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Toast.makeText(context, "Registered Successfully", Toast.LENGTH_SHORT)
+                                .show()
+                            navControl.navigate(R.id.action_signUpFragment_to_homeFragment)
+                        } else {
+                            Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    binding.progressBar.visibility=View.GONE
+                }
+                else{
+                    Toast.makeText(context,"Password and Confirm Password mismatch", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(context,"Please fill all the fields", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
